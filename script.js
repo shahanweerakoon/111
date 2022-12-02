aa04d85a6658448e4cb12450ca8c19781cdddf99
shahanweerakoon <89102713+shahanweerakoon@users.noreply.github.com>
2022-12-02 23:26:30 +0530

    up2

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function name(){
-    console.log("test passed");
-    const para = document.createElement("p");
-para.innerText = "This is a paragraph";
-document.body.appendChild(para);
+idv = 1
+function myFunction() {
+  const inp = document.getElementById("myText").value;
+  console.log("hello");
+  if(inp != ""){
+   
+
+  document.getElementById("myText").value = "";
+
+  const sec = document.createElement("dev");
+  sec.classList.add("mystyle")
+  sec.id= idv;
+  document.body.appendChild(sec);
+
+  const para = document.createElement("p");
+  para.innerText = inp;
+  para.classList.add("fnt");
+  // 
+  //document.body.appendChild(para);
+  document.getElementById(idv).appendChild(para);
+  }
+   idv = idv+1;
 }
-const para = document.createElement("p");
-para.innerText = "This is a paragraph";
-document.body.appendChild(para);
