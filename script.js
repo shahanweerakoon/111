31243e776807eba66b2c7425ff5f591cfd606315
shahanweerakoon <89102713+shahanweerakoon@users.noreply.github.com>
2022-12-02 19:10:48 +0530

    up1

---
+++ script.js
@@ -0,0 +1,9 @@
+function name(){
+    console.log("test passed");
+    const para = document.createElement("p");
+para.innerText = "This is a paragraph";
+document.body.appendChild(para);
+}
+const para = document.createElement("p");
+para.innerText = "This is a paragraph";
+document.body.appendChild(para);
