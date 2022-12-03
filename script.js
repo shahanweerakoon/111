93c383883c3eb284506d07502b9f3d81bba2997e
shahanweerakoon <89102713+shahanweerakoon@users.noreply.github.com>
2022-12-03 09:29:58 +0530

    upp4

--- script.js
+++ script.js
@@ -21,3 +21,13 @@ function myFunction() {
   }
    idv = idv+1;
 }
+
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+});
